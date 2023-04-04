80b66ae8b521360579d42975f222f49d2d2c5a68
= <=>
2023-04-04 12:16:08 +0000

    updated with axios api

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
@@ -11,6 +12,7 @@ const Create = () => {
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [isPending, setIsPending] = useState(false);
+    const [buttonText, setButtonText] = useState('Add Task');
     const history = useHistory();
 
     const handleSubmit=(e) =>{
@@ -18,6 +20,7 @@ const Create = () => {
 
         const task = {title, description,startDate, endDate};
         setIsPending(true);
+        setButtonText("Adding task...");
         // console.log(task); return false;
         const data = {
             title : title,
@@ -26,38 +29,52 @@ const Create = () => {
             enddateTime: endDate
         };
 
-        fetch('http://localhost:8081/todo-api/public/api/v1/todos',{
-            method: 'POST',
-            headers: {"Content-Type" : "application/json"},
-            body: JSON.stringify(data)
-        }).then((response)=>{
-            console.log(response.body);
-            if(!response.ok){
-                var data = response.json();
-                console.log(data);
-                // var message = data.message;
-                // console.log(message)
-                // alert('the error is here now');
-                throw Error('could not fetch the data for that resource');
-            }
-            // if(!response.status === 417){
-            //     response.json();
-            // }
-            return response.json();
-        }).then((data)=>{
-            // console.log(data.message)
-            // console.log(JSON.parse(data.message))
-            history.push('/');
-        }).catch((error)=>{
-            // alert(error.message);
-            // alert()
-        })
+        // fetch('http://localhost:8081/todo-api/public/api/v1/todos',{
+        //     method: 'POST',
+        //     headers: {"Content-Type" : "application/json"},
+        //     body: JSON.stringify(data)
+        // }).then((response)=>{
+        //     console.log(response.body);
+        //     if(!response.ok){
+        //         var data = response.json();
+        //         console.log(data);
+        //         // var message = data.message;
+        //         // console.log(message)
+        //         // alert('the error is here now');
+        //         throw Error('could not fetch the data for that resource');
+        //     }
+        //     // if(!response.status === 417){
+        //     //     response.json();
+        //     // }
+        //     return response.json();
+        // }).then((data)=>{
+        //     // console.log(data.message)
+        //     // console.log(JSON.parse(data.message))
+        //     history.push('/');
+        // }).catch((error)=>{
+        //     // alert(error.message);
+        //     // alert()
+        // })
 
         // fetch('http://localhost:8081/todo-api/public/api/v1/todos',{
         //     method: 'GET'
         // }).then((response)=>{
         //     console.log(response.json());
         // })
+
+        //code for base url
+        const baseURL = "http://localhost:8081/todo-api/public/api/v1/todos"; 
+
+        //code for axios to hit the apis
+        axios.post(baseURL, data)
+             .then((response) => {
+                if(response.responseCode === 417){
+                    setIsPending(false)
+                    alert("error in adding new task");
+                }
+                alert("New task added succesfully");
+                history.push('/');
+             })
 
 
     }
@@ -94,8 +111,8 @@ const Create = () => {
                 type="datetime-local"
                 value={endDate}
                 onChange={(e)=>setEndDate(e.target.value)}/>
-                {!isPending && <button type="submit" >Add Task</button>}
-                {isPending && <button type="submit">Adding task...</button>}
+                {!isPending && <button type="submit" >{buttonText}</button>}
+                {isPending && <button type="submit">{buttonText}</button>}
             </form>
         </div>
      );
