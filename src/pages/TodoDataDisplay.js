f1d255bd9fa02785c0c50180c2f56dfbd9b4e5a2
Joshua Amarfio <53336997+Amarfio@users.noreply.github.com>
2023-12-04 18:12:43 +0000

    updated the start and end date with correct variables to populate the db

--- src/pages/TodoDataDisplay.js
+++ src/pages/TodoDataDisplay.js
@@ -3,16 +3,25 @@ import { Link } from "react-router-dom";
 
 const TodoDataDisplay = (props) => {
   const todo = props.details;
+  const [isPending, setIsPending]= useState(false);
+  const [buttonText, setButtonText] = useState("Update Status");
   //   const [startDateTimeValue, setStartDateTimeValue] = useState("");
   //   const [endDateTimeValue, setEndDateTimeValue] = useState("");
   //   const title = props.title;
   //   console.log(todo.startDateTime);
   const startDateTimeValue = new Date(todo.startDateTime);
   const endDateTimeValue = new Date(todo.endDateTime);
+
   //   console.log(newDate.toLocaleString());
   //   setStartDateTimeValue(newDate.toLocaleString());
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    // const 
+  }
   return (
-    <>
+    <form onSubmit={""}>
       <label>Title</label>
       <input
         type="text"
@@ -41,7 +50,18 @@ const TodoDataDisplay = (props) => {
         value={endDateTimeValue.toLocaleString()}
         // onChange={(e) => setEndDate(e.target.value)}
       />
-    </>
+
+    <label>Status:</label>
+      <select id="">
+        <option value=""></option>
+        <option value="1">in progress</option>
+        <option value="2">completed</option>
+        {/* <option value="3"></option> */}
+      </select>
+
+        {!isPending && <button type="submit">{buttonText}</button>}
+        {isPending && <button type="submit">{buttonText}</button>}
+    </form>
   );
 };
 
